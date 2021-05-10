1c40db5bab82bc2b799f3cac4368800743445691
harishPadmanabh-accubits <harish.p@accubits.com>
2021-05-10 11:45:09 +0530

    add logging ext fun

--- app/src/main/java/com/accubits/mvvm_starter/MainActivity.kt
+++ app/src/main/java/com/accubits/mvvm_starter/MainActivity.kt
@@ -2,6 +2,8 @@ package com.accubits.mvvm_starter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.accubits.mvvm_starter.extensions.LoggingKeys
+import com.accubits.mvvm_starter.extensions.doLogWithTag
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/accubits/mvvm_starter/extensions/ActivityExtensions.kt
+++ app/src/main/java/com/accubits/mvvm_starter/extensions/ActivityExtensions.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProviders
 import com.accubits.mvvm_starter.utils.Injection
+import timber.log.Timber
+import java.lang.Exception
 
 fun <T : ViewModel> AppCompatActivity.obtainViewModel(viewModelClass: Class<T>) =
     ViewModelProviders.of(this).get(viewModelClass)
@@ -11,3 +13,19 @@ fun <T : ViewModel> AppCompatActivity.obtainViewModel(viewModelClass: Class<T>) 
 fun <T : ViewModel> AppCompatActivity.obtainViewModelWithFactory(viewModelClass: Class<T>) =
         ViewModelProviders.of(this,
                 Injection.provideViewModelFactory(application!!, applicationContext)).get(viewModelClass)
+
+fun AppCompatActivity.doLogWithTag(tag:String,message:String,type:LoggingKeys = LoggingKeys.ERROR){
+    try{
+        when(type){
+            LoggingKeys.DEBUG->Timber.tag(tag).d(message)
+            LoggingKeys.ERROR->Timber.tag(tag).e(message)
+            LoggingKeys.INFO->Timber.tag(tag).i(message)
+        }
+    }catch (e:Exception){
+          Timber.e("Caught in doLogWithTag $e")
+    }
+}
+
+enum class LoggingKeys{
+    DEBUG,INFO,ERROR
+}
