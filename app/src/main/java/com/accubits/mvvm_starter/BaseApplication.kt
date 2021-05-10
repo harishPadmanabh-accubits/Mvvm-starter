6359ca9812917a38157cadbb780c6004a568375c
harishPadmanabh-accubits <harish.p@accubits.com>
2021-05-10 11:34:13 +0530

    add timber

---
+++ app/src/main/java/com/accubits/mvvm_starter/BaseApplication.kt
@@ -0,0 +1,14 @@
+package com.accubits.mvvm_starter
+
+import android.app.Application
+import timber.log.Timber
+
+class BaseApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if(BuildConfig.DEBUG)
+            Timber.plant(Timber.DebugTree())
+
+
+    }
+}
