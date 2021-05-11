22b98c521cfcffb7d149df736cf6059d4dbea65e
harishPadmanabh-accubits <harish.p@accubits.com>
2021-05-12 00:27:05 +0530

    add preferences handler

--- app/src/main/java/com/accubits/mvvm_starter/data/network/ApiManager.kt
+++ app/src/main/java/com/accubits/mvvm_starter/data/network/ApiManager.kt
@@ -12,7 +12,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 //pass either Application or context scopes in constructor . Use it from Repositories
 class ApiManger(appContext: Context) {
-    var api : ApiManger ?= null
+    var api : ApiServices ?= null
     init{
         val gson = GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
             .create()
@@ -29,7 +29,7 @@ class ApiManger(appContext: Context) {
             .addConverterFactory(GsonConverterFactory.create(gson))
             .client(okHttpClient)
             .build()
-        api = restAdapter.create(ApiManger::class.java)
+        api = restAdapter.create(ApiServices::class.java)
 
 
 
